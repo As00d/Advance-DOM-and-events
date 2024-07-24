45ae3dcc43dd007c94e67b2f8d768558f5b5a154
Ankita Sood <ankisood@amazon.com>
2024-07-25 00:42:25 +0530

    Section : Create, remove and select elements

--- script.js
+++ script.js
@@ -41,11 +41,43 @@ document.addEventListener('keydown', function (e) {
 
 // NOTES 
 
+// SELECTING ELEMENT IN DOM
 // How would you select the html document ?
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 // to select any other element 
-console.log( document.querySelector('.header'));
+const header = document.querySelector('.header');
 // to select multiple elements we should use querySelectorALL and it will return nodeList
 const section = document.querySelectorAll('.section');
+console.log(section);
+
+document.getElementById('section--1');
+// This method basically returns an html collection that is different from nodeList as html collection is so called life collection. It means if there is any change in dom the value changes automatically
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+// This also returns live html collection
+console.log(document.getElementsByClassName('modal__form'));
+
+// CREATING AND INSERTING ELEMENTS IN DOM
+// .insertAdjacentElement
+const element = document.createElement('div');
+element.classList.add('cookie-message');
+// element.textContent = 'hello there we are a div'
+element.innerHTML = `We use cookie for improved experience <button class='btn--close-cookie btn'> Click Okay <button>`;
+
+// we can also append as the last child ie append - when we add this functionality we were only able to add it at the end and the prepend went away? why this happenned we can not create multiple copies of same element. In order to achieve the same we need to create a copy and then add. Lets see how
+
+// header.prepend(element);
+// header.append(element.cloneNode(true))
+
+header.append(element);
+
+// DELETING THE ELEMENT
+// lets delete the element when someone press okay
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  element.remove();
+  // initially the remove button was not available so people used the parent element and this is called dom traversing
+
+  // element.parentElement.removeChild(element);
+})
